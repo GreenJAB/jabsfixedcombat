000139f6cfbad9e03b8c9bb2c79b645431614a19
joshua <joshua@baikie.org>
2026-09-25 19:47:24 +1000

    bug fixes since rc1

--- src/main/java/net/greenjab/jabsfixedcombat/mixin/food/ServerPlayerMixin.java
+++ src/main/java/net/greenjab/jabsfixedcombat/mixin/food/ServerPlayerMixin.java
@@ -1,19 +1,13 @@
 package net.greenjab.jabsfixedcombat.mixin.food;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.authlib.GameProfile;
 import net.greenjab.jabsfixedcombat.JabsFixedCombat;
 import net.greenjab.jabsfixedcombat.network.SyncHandler;
-import net.greenjab.jabsfixedcombat.registry.registries.GameRuleRegistry;
-import net.greenjab.jabsfixedcombat.util.ModTags;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Mixin;
@@ -70,28 +64,4 @@ public abstract class ServerPlayerMixin extends Player {
         if (player.getLastHurtMobTimestamp() != this.tickCount) player.attackStrengthTicker = (int)(player.getCurrentItemAttackStrengthDelay()/2.0);
     }
 
-    @Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At(value = "RETURN"))
-    private void itemsOnGroundForLonger(ItemStack itemStack, boolean randomly, boolean thrownFromHand, CallbackInfoReturnable<ItemEntity> cir,
-                                        @Local ItemEntity entity) {
-        if (!thrownFromHand && entity != null) {
-            int ticks = this.level().getGameRules().get(GameRuleRegistry.ITEM_DEATH_DESPAWN_TIME)*20*60;
-            if (ticks == 0) entity.setUnlimitedLifetime();
-            else entity.age = 6000-ticks;
-        }
-    }
-
-    @ModifyExpressionValue(method = "restoreFrom", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/world/level/gamerules/GameRules;get(Lnet/minecraft/world/level/gamerules/GameRule;)Ljava/lang/Object;"))
-    private <T> T noDropSpecialItems(T original, @Local(argsOnly = true) ServerPlayer oldPlayer) {
-        if (!oldPlayer.level().getGameRules().get(GameRuleRegistry.PARTIAL_KEEP_INVENTORY)) return original;
-        if ((boolean) original || oldPlayer.isSpectator()) {
-            return original;
-        } else {
-            for (int i = 0; i < this.getInventory().getContainerSize(); i++) {
-                if (oldPlayer.getInventory().getItem(i).is(ModTags.PARTIAL_KEEP_INVENTORY))
-                    this.getInventory().setItem(i, oldPlayer.getInventory().getItem(i));
-            }
-        }
-        return original;
-    }
 }
